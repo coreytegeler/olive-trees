5ed4db9cdde08edda1df0e05912d244a035a9060
coreytegeler <coreytegeler@gmail.com>
2019-01-22 15:46:16 -0500

    remove old templates

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,20 +1,60 @@
 <?php
 snippet('header');
-	$cells = new Pages();
-	if( $artworks = $pages->find( 'artwork' ) ) {
-		$cells->add( $artworks->children()->visible() );
-	}
-	if( $texts = $pages->find( 'texts' ) ) {
-		$cells->add( $texts->children()->visible() );
-	}
-	if( $posts = $pages->find( 'blog' ) ) {
-		$cells->add( $posts->children()->visible() );
-	}
-	if( $events = $pages->find( 'events' ) ) {
-		$cells->add( $events->children()->visible() );
-	}
-	if( $cells ) {
-		snippet( 'cells', array( 'cells' => $cells ) );
-	}
+$title = $site->title();
+$intro = $page->intro()->kirbytext();
+$slides = $site->pages()->template( 'slide' );
+
+echo '<div class="content">';
+  echo '<div class="carousel">';
+    echo '<div class="slides">';
+      // echo '<div class="slide intro current" data-slug="intro">';
+      //   echo '<div class="scroll">';
+      //     echo '<div class="content">';
+      //       echo '<div class="inner">';
+      //         echo '<h1>' . $title . '</h1>';
+      //         echo '<div class="body">';
+      //           echo $intro;
+      //         echo '</div>';
+      //       echo '</div>';
+      //     echo '</div>';      
+      //   echo '</div>';
+      // echo '</div>';
+      if( $slides ) {
+        foreach ( $slides as $index => $slide ) {
+          echo '<div class="slide" data-slug="slide-' . $index . '">';
+            echo '<div class="scroll">';
+
+              echo '<div class="content">';
+                echo '<div class="inner">';
+                  echo '<div class="body">';
+
+                  if( $slide->hasImages() ) {
+                    foreach( $slide->images() as $image ) {
+                      echo '<figure>';
+                        echo $image;
+                        $caption = $image->caption();
+                        if( $caption->isNotEmpty() ) {
+                          echo '<figcaption>' . $image->caption()->kirbytext() . '</figcaption>';
+                        }
+                      echo '</figure>';
+                    }
+                  } elseif( $slide->text() ) {
+                    $text = $slide->text();
+                    echo $text->kirbytext();
+                  }
+
+                  echo '</div>';
+                echo '</div>';
+              echo '</div>';
+
+            echo '</div>';
+          echo '</div>';
+        }
+      }
+    echo '</div>';
+    echo '<div class="arrow left" data-direction="left"></div>';
+    echo '<div class="arrow right" data-direction="right"></div>';
+  echo '</div>';
+echo '</div>';
 snippet('footer');
 ?>
--- site/templates/text.php
+++
@@ -1,7 +0,0 @@
-<?php
-snippet( 'header' );
-	echo '<div class="content">';
-	  snippet( 'content/text' );
-	 echo '</div>';
-snippet( 'footer' );
-?>
